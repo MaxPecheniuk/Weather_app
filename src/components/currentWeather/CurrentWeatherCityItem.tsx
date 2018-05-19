e7847bab50650a07af3f73842de228735bc576fb
Maxim Pechenyuk <plitochnik@gmail.com>
2018-05-19 14:27:31 +0300

    finish home page style

--- src/components/currentWeather/CurrentWeatherCityItem.tsx
+++ src/components/currentWeather/CurrentWeatherCityItem.tsx
@@ -3,49 +3,59 @@ import { apiConfigs } from '../../configs/apiConfigs';
 import { CurrentWeatherTypes } from '../../types/currentWeather.types';
 
 import './CurrentWeatherCityItem.scss';
+
 interface CityItemProps {
   weatherData: CurrentWeatherTypes;
-  // weatherIcon: string;
-  // weatherDescription: string;
 }
 
 export const CurrentWeatherCityItem: React.SFC<CityItemProps> = (props: CityItemProps) => {
   return (
     <div className="city-list-item">
-
       <div className="city-list-item__main">
-
         <div className="city-list-item__main__city-name">
           {props.weatherData.name}
         </div>
-
-        {props.weatherData.weather.map((item, i) => {
-          return(
-            <div key={i} className="city-list-item__main__weather-condition">
-              <img  src={apiConfigs.conditionIconUrl + item.icon + '.png'} alt=""/>
-              <div className="city-list-item__main__weather-condition__description">
-                {item.description}
+        <div className="city-list-item__main__weather-data">
+          {props.weatherData.weather.map((item, i) => {
+            return (
+              <div key={i} className="city-list-item__main__weather-condition">
+                <div className="city-list-item__main__weather-condition__description">
+                  {item.description}
+                </div>
+                <img src={apiConfigs.conditionIconUrl + item.icon + '.png'} alt=""/>
               </div>
-            </div>
-          );
-        })}
-
-        <div className="city-list-item__current-temp">
-          {Math.round(props.weatherData.main.temp)}°C
+            );
+          })}
+          <div className="city-list-item__main__current-temp">
+            {Math.round(props.weatherData.main.temp)}°C
+          </div>
         </div>
       </div>
-
-        <div className="city-list-item_details">
-
-          <div className="city-list-item__humidity">
-            Humidity: {props.weatherData.main.humidity}%
-          </div>
-
-          <div className="city-list-item__wind">
-            Wind: {props.weatherData.wind.speed} km/h
+      <div className="city-list-item__details">
+        <div className="city-list-item__details-item">
+          <img className="weather-icon" src={require('../../assets/humidity.svg')} alt=""/>
+          <div className="city-list-item__details-item__text-block">
+            <div className="text-block__name">Humidity</div>
+            <div className="text-block__value">{props.weatherData.main.humidity}%</div>
           </div>
         </div>
-
+        <div className="city-list-item__details-item">
+          <img className="weather-icon" src={require('../../assets/pressure.svg')} alt=""/>
+          <div className="city-list-item__details-item__text-block">
+            <div className="text-block__name">Pressure</div>
+            <div className="text-block__value">{props.weatherData.main.pressure}hPa</div>
+          </div>
+        </div>
+        <div className="city-list-item__details-item">
+          <img className="weather-icon" src={require('../../assets/wind.svg')} alt=""/>
+          <div className="city-list-item__details-item__text-block">
+            <div className="text-block__name">Wind</div>
+            <div className="text-block__value">
+              {props.weatherData.wind.speed}km/h
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
